675be0748f5e0001260a5f5cc017845957ee4f5a
eperez <eperez@stemdo.io>
2024-06-15 09:54:19 +0200

    clave publica

--- terraform/main.tf
+++ terraform/main.tf
@@ -129,7 +129,7 @@ resource "azurerm_linux_virtual_machine" "vm" {
 
   admin_ssh_key {
     username   = each.value.admin_username
-    public_key = file("C:/Users/eperez/.ssh/id_rsa.pub")
+    public_key = file("./id_rsa.pub")
   }
 }
 
